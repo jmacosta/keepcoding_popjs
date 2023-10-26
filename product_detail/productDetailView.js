88594ae93a7f4d70922ad871aed30414a7f8888f
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-10-26 20:37:47 +0200

    WIP product details

--- product_detail/productDetailView.js
+++ product_detail/productDetailView.js
@@ -10,24 +10,57 @@ export const buildProductDetailCard = product => {
         alt: 'search icon',
         text: 'Se busca'
       };
-  return `<figure>
-          <img
-            class="product_image"
-            src="${product.image}"
-            alt="${product.name}"
-          />
-        </figure>
-        <div class="text-content">
-          <div class="product_name">${product.name}</div>
-          <div class="product_description">
-            <p>
-              ${product.description}
-            </p>
-          </div>
-          <div class="sell_or_search">
-            <img src="${sellOrSeachIcon.image}" onerror="this.src='assets/no-image.jpg';" alt="${sellOrSeachIcon.alt}" />
-            <span>${sellOrSeachIcon.text}</span>
-          </div>
-          <div class="product_price">${product.price}€</div>
-        </div>`;
+  return `
+  <section id="header_product_detail"><nav>
+    <svg class="arrow" version="1.1" viewBox="0 0 512 512" width="512px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><polygon points="352,115.4 331.3,96 160,256 331.3,416 352,396.7 201.5,256 " stroke="#727272"/></svg>
+    <span>Volver a inicio</span>
+    
+  </nav>
+  <div class="sell_or_search">
+             <img src="${
+               sellOrSeachIcon.image
+             }" onerror="this.src='assets/no-image.jpg';" alt="${
+    sellOrSeachIcon.alt
+  }" />
+            <!--<span>${sellOrSeachIcon.text}</span>-->
+           </div></section>
+  
+   
+  <div class="photo">
+    <img src="${
+      product.image
+    }" onerror="this.src='assets/no-image.jpg';" alt="${product.name}" />
+  </div>
+  <div class="description">
+    <h2>${product.name}</h2>
+    <h4>${product.tags.join(', ')}</h4>
+    <h1>${product.price}€</h1>
+    <p>${product.description}</p>
+    <div id ="authorized_buttons">
+    <button>Modificar</button>
+    <button class="delete_product">Eliminar</button>
+    </div>
+  </div>
+`;
 };
+
+// `<figure>
+//           <img
+//             class="product_image"
+//             src="${product.image}"
+//             alt="${product.name}"
+//           />
+//         </figure>
+//         <div class="text-content">
+//           <div class="product_name">${product.name}</div>
+//           <div class="product_description">
+//             <p>
+//               ${product.description}
+//             </p>
+//           </div>
+//           <div class="sell_or_search">
+//             <img src="${sellOrSeachIcon.image}" onerror="this.src='assets/no-image.jpg';" alt="${sellOrSeachIcon.alt}" />
+//             <span>${sellOrSeachIcon.text}</span>
+//           </div>
+//           <div class="product_price">${product.price}€</div>
+//         </div>`;
