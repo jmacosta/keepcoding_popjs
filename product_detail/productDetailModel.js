1f014beeb6b504191bef8b2097b91c6db11c299d
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-10-28 21:35:55 +0200

    refactor for apply api method

--- product_detail/productDetailModel.js
+++ product_detail/productDetailModel.js
@@ -1,32 +1,18 @@
+import { apiMethods } from '../utils/apiMethods.js';
 export const getProductDetail = async productId => {
-  const url = `http://127.0.0.1:8000/api/products?id=${productId}&_expand=user`;
-  let response;
   try {
-    response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    const data = await response.json();
+    const response = await apiMethods().get(
+      `api/products?id=${productId}&_expand=user`
+    );
 
     if (!response.ok) {
       throw new Error('no data');
     }
     if (response.ok) {
+      const data = await response.json();
       return data[0];
     }
   } catch (error) {
     throw new Error('error al extraer');
   }
 };
-
-// id: 4,
-//     userId: 5,
-//     name: 'Casco de Motocicleta de Carrera',
-//     sellOrSearch: true,
-//     price: 129.99,
-//     image: 'images/cascocarrera.jpg',
-//     tags: ['motor'],
-//     description:
-//       'Casco de motocicleta de carreras para máxima seguridad en la pista.'
