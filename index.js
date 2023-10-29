cf08bf38cf81fae3facc4798fa571c27f64870bd
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-10-29 15:21:35 +0100

    add loader listener

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+import { loaderController } from './loader/loaderController.js';
 import { productListController } from './product_list/productListController.js';
 import { sessionController } from './session/sessionController.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const session = document.querySelector('#session');
-  sessionController(session);
   const productList = document.querySelector('#products_list');
+  const loader = document.querySelector('#loader');
+  sessionController(session);
+  const { show, hide } = loaderController(loader);
+  productList.addEventListener('startLoadingProducts', () => {
+    show();
+  });
+  productList.addEventListener('finishLoadingProducts', () => {
+    hide();
+  });
+
   productListController(productList);
 });
