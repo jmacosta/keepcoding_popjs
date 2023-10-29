c54bf2bc5b01621b311e5b076de6c308023568fa
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-10-29 20:40:46 +0100

    Add loader feature to product_detail

--- product_detail/index.js
+++ product_detail/index.js
@@ -1,10 +1,21 @@
+import { loaderController } from '../loader/loaderController.js';
 import { sessionController } from '../session/sessionController.js';
 import { productDetailController } from './productDetailController.js';
+
 const params = new URLSearchParams(window.location.search);
 const productID = params.get('id');
 document.addEventListener('DOMContentLoaded', () => {
   const session = document.querySelector('#session');
-  sessionController(session);
   const productDetail = document.querySelector('.card');
+  const loader = document.querySelector('#loader');
+  const { show, hide } = loaderController(loader);
+  sessionController(session);
+  productDetail.addEventListener('startLoadingProductDetail', () => {
+    show();
+  });
+  productDetail.addEventListener('finishLoadingProductDetail', () => {
+    hide();
+  });
+
   productDetailController(productDetail, productID);
 });
--- product_detail/productDetailController.js
+++ product_detail/productDetailController.js
@@ -1,12 +1,15 @@
 import { authorizedButtonsController } from '../authorizedButtons/authorizedButtonsController.js';
 import { goHomeController } from '../goHome/goHomeController.js';
+import { dispatchEvent } from '../utils/dispatchEvents.js';
 import { getProductDetail } from './productDetailModel.js';
 import { buildProductDetailCard } from './productDetailView.js';
 
-export const productDetailController = async (productCard, productId) => {
+export const productDetailController = async (productDetail, productId) => {
+  dispatchEvent('startLoadingProductDetail', null, productDetail);
   const product = await getProductDetail(productId);
-  productCard.innerHTML = buildProductDetailCard(product);
+  productDetail.innerHTML = buildProductDetailCard(product);
   goHomeController();
   const authorizedButtons = document.querySelector('#authorized_buttons');
   authorizedButtonsController(authorizedButtons, product.userId, productId);
+  dispatchEvent('finishLoadingProductDetail', null, productDetail);
 };
