860f24e94ebe3146c4e9f4f657c1a9bbb490d076
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-10-29 20:40:02 +0100

    Add loader feature to product create

--- product_create/index.js
+++ product_create/index.js
@@ -1,4 +1,15 @@
+import { loaderController } from '../loader/loaderController.js';
 import { productCreateController } from './productCreateController.js';
 
-const productCreateForm = document.querySelector('#product_create');
-productCreateController(productCreateForm);
+document.addEventListener('DOMContentLoaded', () => {
+  const productCreateForm = document.querySelector('#product_create');
+  const loader = document.querySelector('#loader');
+  const { show, hide } = loaderController(loader);
+  productCreateForm.addEventListener('startCreateProduct', () => {
+    show();
+  });
+  productCreateForm.addEventListener('finishCreateProduct', () => {
+    hide();
+  });
+  productCreateController(productCreateForm);
+});
--- product_create/productCreateController.js
+++ product_create/productCreateController.js
@@ -1,3 +1,4 @@
+import { dispatchEvent } from '../utils/dispatchEvents.js';
 import { createProduct } from './productCreateModel.js';
 export const productCreateController = productCreateForm => {
   productCreateForm.addEventListener('submit', event => {
@@ -14,7 +15,9 @@ export const productCreateController = productCreateForm => {
 const validateForm = async productCreateForm => {
   const dataForm = new FormData(productCreateForm);
   try {
+    dispatchEvent('startCreateProduct', null, productCreateForm);
     await createProduct(dataForm);
+    dispatchEvent('finishCreateProduct', null, productCreateForm);
     alert('Ya tenemos otra cosa pa mostrar');
     window.location = './index.html';
   } catch (error) {
