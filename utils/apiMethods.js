023f6be6a6bf81f951579bd3f27315059be47744
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2023-10-29 13:11:31 +0100

    add create product method

--- utils/apiMethods.js
+++ utils/apiMethods.js
@@ -21,7 +21,31 @@ export const apiMethods = () => {
       }
     });
   };
-  const postProduct = async (endpoint, baseUrl = SERVERURL) => {};
+  const postProduct = async (endpoint, baseUrl = SERVERURL, body) => {
+    const token = localStorage.getItem('token');
+    try {
+      const response = await fetch(baseUrl + endpoint, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+          'Content-type': 'application/json',
+          Authorization: `Bearer ${token}`
+        }
+      });
+
+      if (!response.ok) {
+        const data = await response.json();
+        const message = data.message || 'No ha sido posible borrar el elemento';
+        throw new Error(message);
+      }
+    } catch (error) {
+      if (error.message) {
+        throw error.message;
+      } else {
+        throw error;
+      }
+    }
+  };
   const remove = async (endpoint, baseUrl = SERVERURL) => {
     const token = localStorage.getItem('token');
     try {
